4e23a041288ff9a6b290a406c443487318f755f9
upaneeta <61232529+upaneeta@users.noreply.github.com>
2021-11-13 16:58:08 +0530

    Create Reverse_The_Array.cpp

---
+++ Reverse_The_Array.cpp
@@ -0,0 +1,21 @@
+
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  //Your code here
+  
+  int i;
+  
+  for(i=str.size()-1;i>=0;i--)
+  {
+      
+      cout<<str[i];
+      
+     
+  }
+}
