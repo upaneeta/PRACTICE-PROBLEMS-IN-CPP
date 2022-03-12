18400db8367f7902fdd04dbdcf105eb67da7f109
upaneeta <61232529+upaneeta@users.noreply.github.com>
2022-03-12 13:30:05 +0530

    Create Discus_Throw.cpp

---
+++ Discus_Throw.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here#include<stdio.h>
+
+int i,a,b,c,ans,n;
+cin>>n;
+
+for(i=0;i<n;i++)
+{
+    cin>>a>>b>>c;
+    if(a>=b && a>=c)
+    {
+        ans=a;
+    }
+    else if(b>=a && b>=c)
+    {
+        ans=b;
+    }
+    else if(c>=a && c>b)
+    {
+        ans=c;
+    }
+
+    cout<<ans<<endl;
+
+}
+	return 0;
+}
