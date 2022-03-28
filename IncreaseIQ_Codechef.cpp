1517f67d6fa0d680be0a824971dba8d8e33a1076
upaneeta <61232529+upaneeta@users.noreply.github.com>
2022-03-28 10:37:50 +0530

    Add files via upload

---
+++ IncreaseIQ_Codechef.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    if(num+7>170)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
