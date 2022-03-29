50d0fe5a7ab58922d124dde594a961f8aeeb76be
upaneeta <61232529+upaneeta@users.noreply.github.com>
2022-03-29 16:44:46 +0530

    Add files via upload

---
+++ TheThreeTopics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if(d==a || d==b ||d==c)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
